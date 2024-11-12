e29727571ff8832c5dae85efd375b2e62e3e70f3
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-11-12 12:53:52 +0900

    [241112]

--- 01_lecture-source/chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/ClassMappingController.java
+++ 01_lecture-source/chap01-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestmapping/ClassMappingController.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.requestmapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,6 +36,29 @@ public class ClassMappingController {
         return "mappingResult";
     }
 
-    
+    /* 3. Path Variable  (url 경로를 타고 오는 값) */
+    /* comment.
+    *   @PathVariable 어노테이션을 통해 요청 URL 로부터 변수를 받아 올 수 있다.
+    *   path variable 로 전달 되는 {변수명} 값은 반드시 매개변수명과 일치해야 한다.
+    *   만약 동일하지 않으면 @PathVariable("이름") 을 설정해야 한다.
+    * */
+                    // 변수로 담아야 함
+    @GetMapping("/detail/{orderNo}")
+                                           // URL 에 지정한 변수명과 일치 시켜야 함
+    public String orderDetail(Model model, @PathVariable int orderNo){
 
+        model.addAttribute("message", orderNo + "번 주문 상세 조회 핸들러 메소드 호출됨...");
+
+        return "mappingResult";
+    }
+
+// URL 에 지정한 변수명과 일치 시키고 싶지 않을 때
+//    @GetMapping("/detail/{orderNo}")
+//    public String orderDetail(Model model, @PathVariable("orderNO") int no){
+//
+//        model.addAttribute("message", no + "번 주문 상세 조회 핸들러 메소드 호출됨...");
+//
+//        return "mappingResult";
+//    }
+    
 }
