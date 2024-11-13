135d3a9f257433987e6f28053715afe4c83c433a
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-11-13 10:00:25 +0900

    [241113]

---
+++ 01_lecture-source/chap02-hendler-method-lecture-source/src/test/java/com/ohgiraffers/hendlermethod/Chap02HendlerMethodLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.hendlermethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap02HendlerMethodLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
