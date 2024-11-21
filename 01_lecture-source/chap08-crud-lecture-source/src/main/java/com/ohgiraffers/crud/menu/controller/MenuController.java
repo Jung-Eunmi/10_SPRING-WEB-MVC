d0e5b43fd54b0ae85401beba033b6e7880377acd
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-11-21 14:46:09 +0900

    [241121]

--- 01_lecture-source/chap08-crud-lecture-source/src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
+++ 01_lecture-source/chap08-crud-lecture-source/src/main/java/com/ohgiraffers/crud/menu/controller/MenuController.java
@@ -6,9 +6,8 @@ import com.ohgiraffers.crud.menu.model.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -55,6 +54,15 @@ public class MenuController {
     @ResponseBody
     public List<CategoryDTO> findCategoryList() {
         return menuService.findAllCategory();
+    }
+
+    @PostMapping("regist")
+    public String registMenu(@ModelAttribute MenuDTO newMenu, RedirectAttributes rttr) {
+        /* comment.
+        *   @ModelAttribute : form 태그로 묶어서 넘어오는 값을 클래스 자료형에
+        *   담기위해 작성하는 어노테이션
+        *   RedirectAttributes : 리다이렉트 시 저장할 값이 있으면 사용하는 객체 */
+
     }
 
 }
