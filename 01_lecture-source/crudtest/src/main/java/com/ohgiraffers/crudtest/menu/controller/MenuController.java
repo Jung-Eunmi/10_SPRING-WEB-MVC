ee83eec6aad4208fe94722cf34dd6dfc668148c9
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-11-28 09:27:17 +0900

    [241128]

--- 01_lecture-source/crudtest/src/main/java/com/ohgiraffers/crudtest/menu/controller/MenuController.java
+++ 01_lecture-source/crudtest/src/main/java/com/ohgiraffers/crudtest/menu/controller/MenuController.java
@@ -4,22 +4,27 @@ import com.ohgiraffers.crudtest.menu.model.dto.CategoryDTO;
 import com.ohgiraffers.crudtest.menu.model.dto.MenuDTO;
 import com.ohgiraffers.crudtest.menu.model.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/menu/*")
 public class MenuController {
 
     private final MenuService menuService;
+    private final MessageSource messageSource;
 
     @Autowired
-    public MenuController(MenuService menuService) {
+    public MenuController(MenuService menuService, @Qualifier("messageSource") MessageSource messageSource) {
         this.menuService = menuService;
+        this.messageSource = messageSource;
     }
 
     // 전체 메뉴 조회
@@ -59,12 +64,16 @@ public class MenuController {
     }
 
     @PostMapping("insert")
-    public String insertMenu(@ModelAttribute MenuDTO menu) {
+    public String insertMenu(@ModelAttribute MenuDTO menu, RedirectAttributes rttr, Locale locale) {
 
         menuService.insertMenu(menu);
 
         return "redirect:/menu/all";
     }
+
+    // 메뉴 삭제
+    @GetMapping("delete")
+    public void delete(){}
 
 
 
